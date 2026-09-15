fbab9c95fbf3f8b00d4bb5482adf6e83d99f0b71
pacsss <paccanaro18@gmail.com>
2026-09-15 18:43:19 -0300

    feat: adiciona repositorio JPA para Transacao

---
+++ src/main/java/com/paccanaro/fintech/conta/TransacaoRepository.java
@@ -0,0 +1,7 @@
+package com.paccanaro.fintech.conta;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.UUID;
+
+public interface TransacaoRepository extends JpaRepository<Transacao, UUID> {
+}
