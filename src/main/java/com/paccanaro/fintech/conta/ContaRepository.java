4463a60dd1d70906771b1e63d178bce9da120d18
pacsss <paccanaro18@gmail.com>
2026-09-15 18:53:03 -0300

    fix: adiciona metodo findByUsuarioEmail ausente no commit anterior

--- src/main/java/com/paccanaro/fintech/conta/ContaRepository.java
+++ src/main/java/com/paccanaro/fintech/conta/ContaRepository.java
@@ -2,7 +2,10 @@ package com.paccanaro.fintech.conta;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ContaRepository extends JpaRepository<Conta, UUID> {
+
+    Optional<Conta> findByUsuarioEmail(String email);
 }
