d15a4527b751abea8f4f901ca3e897dc9acea6de
pacsss <paccanaro18@gmail.com>
2026-09-15 16:58:18 -0300

    feat: adiciona endpoint temporario /me para validar autenticacao jwt

--- src/main/java/com/paccanaro/fintech/usuario/UsuarioController.java
+++ src/main/java/com/paccanaro/fintech/usuario/UsuarioController.java
@@ -7,10 +7,8 @@ import com.paccanaro.fintech.usuario.dto.UsuarioResponse;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/usuarios")
