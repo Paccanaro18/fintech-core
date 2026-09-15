6451250ad42762c4c5677046b0652253293195ac
pacsss <paccanaro18@gmail.com>
2026-09-15 14:32:03 -0300

    feat: adiciona endpoint POST /usuarios para cadastro

---
+++ src/main/java/com/paccanaro/fintech/usuario/UsuarioController.java
@@ -0,0 +1,31 @@
+package com.paccanaro.fintech.usuario;
+
+import com.paccanaro.fintech.usuario.dto.CadastroRequest;
+import com.paccanaro.fintech.usuario.dto.UsuarioResponse;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+
+   private final UsuarioService usuarioService;
+
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    @PostMapping
+    public ResponseEntity<UsuarioResponse> cadastrarUsuario(@RequestBody @Valid CadastroRequest request) {
+
+        Usuario usuario = usuarioService.cadastrar(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(UsuarioResponse.fromEntity(usuario));
+    }
+
+
+}
