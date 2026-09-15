1979a712e1a9ea5c6de5bf120ad94a66279c8453
pacsss <paccanaro18@gmail.com>
2026-09-15 18:43:25 -0300

    feat: adiciona endpoints de deposito, saque e consulta de saldo

---
+++ src/main/java/com/paccanaro/fintech/conta/ContaController.java
@@ -0,0 +1,39 @@
+package com.paccanaro.fintech.conta;
+
+import com.paccanaro.fintech.conta.dto.ContaResponse;
+import com.paccanaro.fintech.conta.dto.DepositoRequest;
+import com.paccanaro.fintech.conta.dto.SaqueRequest;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/contas")
+public class ContaController {
+
+    private final ContaService contaService;
+
+    public ContaController(ContaService contaService) {
+        this.contaService = contaService;
+    }
+
+    @PostMapping("/depositar")
+    public ResponseEntity<ContaResponse> depositar(@Valid @RequestBody DepositoRequest request) {
+        Conta conta = contaService.depositar(request);
+        return ResponseEntity.ok(ContaResponse.fromEntity(conta));
+    }
+
+    @PostMapping("/sacar")
+    public ResponseEntity<ContaResponse> sacar(@Valid @RequestBody SaqueRequest request) {
+        Conta conta = contaService.sacar(request);
+        return ResponseEntity.ok(ContaResponse.fromEntity(conta));
+    }
+
+    @GetMapping
+    public ResponseEntity<ContaResponse> visualizarSaldo() {
+        Conta conta = contaService.buscarContaLogado();
+        return ResponseEntity.ok(ContaResponse.fromEntity(conta));
+    }
+
+}
